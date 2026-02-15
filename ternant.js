509c9e810ed7c579ea512f4fe6f02effdcb6b5bb
R-tech solutions <Dan570140@gmail.com>
2026-02-15 16:40:49 +0300

    Update ternant.js

--- ternant.js
+++ ternant.js
@@ -122,3 +122,81 @@ function closeSuccess() {
 
 
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.getElementById("joinform");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const email = document.getElementById("email").value.trim();
+        const phoneNumber = document.getElementById("phoneNumber").value.trim();
+       const textarea = document.getElementById("textarea").value.trim();
+        const password1 = document.getElementById("password1").value;
+        const Password2 = document.getElementById("password2").value;
+        const error = document.getElementById("error");
+       // const successpopup = document.getElementById("successpopup").style.display="flex";
+        
+
+
+
+
+        error.style.color = "red";
+
+        if (email === "" || phoneNumber === ""  || password1 === "" || Password2 === "") {
+           alert("please fill in all the filleds before you submit!")
+            return;
+        }
+
+        if (!email.includes("@") || !email.includes(".")) {
+            error.textContent = "Enter a valid email address.";
+            return;
+        }
+
+        if (phoneNumber.length < 10) {
+         alert("Invalid Number! Phone number must be at least 10 digits.");
+            return;
+
+            
+        }
+
+       if (textarea==="") {
+         alert("Please specify atleast one area of intrest..");
+            return;
+
+
+        if (password.length < 6)|| password.includes("@") {
+            error.textContent = " Create a strong Password with at least 6 characters and a special character '@'.";
+            return;
+        }
+       
+
+        if (password1 !== Password2) {
+            alert( "Passwords do not match.");
+            return;
+        }
+    document.getElementById("successPopup").style.display = "flex";
+
+ return;
+ 
+
+       // error.style.color = "green";
+       // error.textContent = "Validation successful. Processing login...";
+
+        
+        // Example redirect
+        // window.location.href = "tenant-dashboard.html";
+    });
+});
+function showSuccess() {
+    document.getElementById("successPopup").style.display = "flex";
+}
+
+function closeSuccess() {
+    document.getElementById("successPopup").style.display = "none";
+}
+
+
+
